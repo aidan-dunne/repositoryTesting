2d570c121f6f4591f207982981cadced7c78dbbc
Aidan Dunne <aidanmdunne03@gmail.com>
2023-10-30 21:12:25 -0500

    Test commit

---
+++ gradingTests.cpp
@@ -0,0 +1,17 @@
+// gradingTests.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    //testing
+    int num = 5;
+    const int test = num;
+
+    std::cout << test << "\n\n";
+
+    return 0;
+}
